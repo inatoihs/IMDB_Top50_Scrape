123b5189a0dbb41a8bebd186425a2c4a01fd6431
inatoihs <sabanna.taihei@gmail.com>
2021-09-06 23:59:21 +0900

    a

--- IMDB_Top50_Scraper.py
+++ IMDB_Top50_Scraper.py
@@ -13,7 +13,7 @@ if input_year > current_year:
     print("No movie is recorded in year %i yet!!" % (input_year))
 else:
     for year in tqdm(range(input_year, current_year+1)):
-        sys.stdout = open(os.path.join(dataset_location,"IMDB_Top50_" + str(year) + '.txt'), 'w+')
+        sys.stdout = open(os.path.join(dataset_location,"IMDB_Top50_" + str(year) + '.json'), 'w+')
         url = "http://www.imdb.com/search/title?release_date=" + str(year) + "," + str(year) + "&title_type=feature"
         response = requests.get(url,headers=headers)
         soup = BeautifulSoup(response.text, "html.parser")
